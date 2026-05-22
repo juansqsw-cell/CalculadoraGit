2651e9a35e8ebfad8eb73cba701261b9ebe4739a
Jamer <juansqsw@gmail.com>
2026-05-22 14:02:19 -0500

    Agrega funcion resta

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,12 @@
 def suma(a, b):
     return a + b
 
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
 print("Prueba suma:", suma(3, 5))
+print("Prueba resta:", resta(10, 4))
+print("Prueba multiplicacion:", multiplicacion(3, 7))
